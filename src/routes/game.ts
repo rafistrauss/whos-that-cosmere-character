c64118a97005dabe34a29c9977777d68cfb58c30
Rafi Strauss <rafi.strauss@nbcuni.com>
2025-04-28 15:27:53 -0400

    fix: Trim user guesses before adding to the guesses array

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -43,7 +43,7 @@ export class Game {
 	 * true if the guess was correct, false otherwise
 	 */
 	enter(guess: string) {
-		this.guesses.push(guess);
+		this.guesses.push(guess.trim());
 
 		if ([this.primaryAnswer, ...this.alternateAnswers].some(answer => guess.toLowerCase() === answer.toLowerCase())) {
 			return true;
